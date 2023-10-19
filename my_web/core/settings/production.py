a407e7b55ebaee767da0c793f765bb51a6563eb5
GuidoGdR <guidodorego@gmail.com>
2023-10-18 22:47:32 -0300

    Agrego algunas configuraciones para aumentar la seguridad desde la config

--- my_web/core/settings/production.py
+++ my_web/core/settings/production.py
@@ -6,3 +6,9 @@ DEBUG = False
 ALLOWED_HOSTS = ['www.guidodorego.com', 'guidodorego.com']
 
 CSRF_TRUSTED_ORIGINS = ['https://*.guidodorego.com','https://guidodorego.com']
+
+CSRF_COOKIE_SECURE = True
+
+CSRF_USE_SESSIONS = True
+
+SESSION_COOKIE_SECURE = True
