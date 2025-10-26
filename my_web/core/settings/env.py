42f30aa038bfb2f85aa2f98e356454acff9aaca2
Guido do Rego <guidodorego@gmail.com>
2025-10-26 05:44:15 -0300

    Elimino el archivo env.py del gitignore y expongo uno de ejemplo para facilitar configuracion del que use el repo

---
+++ my_web/core/settings/env.py
@@ -0,0 +1,12 @@
+import os
+
+os.environ['SECRET_KEY'] = 'gsdfrgfkodehimttger4534gfhy5gtefuygdwyhbvc5567432bdoibtrekfogiefgitjmde'
+
+os.environ['EMAIL_HOST'] = 'smtp.gmail.com'
+os.environ['EMAIL_PORT'] = '587'
+os.environ['EMAIL_HOST_USER'] = 'mail_de_pagina@gmail.com'
+os.environ['EMAIL_HOST_PASSWORD'] = 'password'
+
+os.environ['SEND_MAIL_TO'] = 'mail_personal@gmail.com'
+
+os.environ['TURNSTILE_SECRET_KEY'] = 'TurnstileKeyCloudflare'
